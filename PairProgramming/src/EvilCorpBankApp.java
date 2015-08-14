4d67866ec255f9671d697ac1570be189da9dbfea
NishaJeffrey <nishakpaul@gmail.com>
2015-08-14 10:14:11 -0400

    Final_Final Commit

--- PairProgramming/src/EvilCorpBankApp.java
+++ PairProgramming/src/EvilCorpBankApp.java
@@ -7,6 +7,7 @@ import java.sql.Statement;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Scanner;
 public class EvilCorpBankApp {
 	
@@ -30,76 +31,102 @@ public class EvilCorpBankApp {
 
 	        //creating PreparedStatement object to execute query
 	        PreparedStatement preStatement = conn.prepareStatement(sql);
-	        ResultSet result = preStatement.executeQuery();
-	       // System.out.println(result);
-	        if(result.next()==false){
+	        ResultSet result =null;
+	        result  = preStatement.executeQuery();	        
+	        //System.out.println(result);
+	        if(result==null || result.next()==false){
 	        	System.out.println("Account does not exist.Do you wish to open an account?(y/n)");
 	        	String ch=sc.nextLine();
+	        	Random r=new Random(); 
+	        	int acnumber=1000+r.nextInt(9999);	        	
+	        	ResultSet rs=null;
 	        	if(ch.equalsIgnoreCase("y")){
-	        		System.out.println("Enter the account num:");
-	        		int acnumber=Integer.parseInt(sc.nextLine());
+	        		do{	        		 
+	        			Statement s=conn.createStatement(); 
+	        			String qury="select acc_num from evilcorpcustomer where acc_num="+acnumber; 
+	        			rs=s.executeQuery(qury);
+	        			acnumber=1000+r.nextInt(9999);
+	        			}while(rs.next()==true); 
+	        		System.out.println("New Account No:="+acnumber);	        		
 	        		System.out.println("Enter the name:");
 	        		String name=sc.nextLine();
 	        		System.out.println("Enter a birth date(mm/dd/yyyy)");
 	        		String birthday=sc.nextLine();
 	        		Statement st = conn.createStatement();
-			        String q="insert into evilcorpcustomer values("+acnumber+",'"+name+"',0,"+"to_date('"+birthday+"','mm/dd/yyyy'))" ; 
-			        System.out.println(q);
+			        String q="insert into evilcorpcustomer values("+acnumber+",'"+name+"',0,"+"to_date('"+birthday+"','mm/dd/yyyy'))" ;			       
 			        st.execute(q);
 			        System.out.println("Congrats!Account Open.");
 	        	}
 	        	
 	        }
 	        else{
-	        Customer cust=null;
-	        while(result.next()){
-	        	  cust=new Customer(result.getInt("acc_num"),result.getString("Name"),result.getInt("start_bal"),result.getDate("birthday"));
-	        	 System.out.println("customer Information:\n\n"+cust);
-	        }
-	        //String sql1="select * from evilcorptrans join evilcorp_type on evilcorptrans.trans_type=evilcorp_type.type_id where user_id='"+acct_number+"'";
-	        String sql1="select * from evilcorptrans where user_id='"+acct_number+"' and trans_processed=0";
-	        //System.out.println(sql1);
-	        PreparedStatement preStatement1 = conn.prepareStatement(sql1);
-	        //System.out.println( preStatement1);
-	        ResultSet  result2 = preStatement1.executeQuery();	       
-	        System.out.println();
-	        while(result2.next()){
-	        	Transaction t=new Transaction(result2.getInt("trans_type"),result2.getDate("trans_date"),result2.getInt("amount"));
-	        	//Transaction t=new Transaction(result2.getString("descript"),result2.getDate("trans_date"),result2.getInt("amount"));
-	        	cust.addToTransactions(t);
-	        	System.out.println(t);	        	 
-        }
-	        cust.updateBalance();
-	        System.out.println("Current balance= "+cust.getBalance());
-           System.out.println("done"); 
-           System.out.println("Do you want to process aother transaction(Y/N):");
-           String choice=sc.nextLine();
-           while(choice.equalsIgnoreCase("y")){
-        	   System.out.println("Enter a transaction type (Check-1, Debit card-2, Deposit-3 or Withdrawal-4 or CloseAccount=5):");
-        	   int type=Integer.parseInt(sc.nextLine());
-        	   System.out.println("Enter the amount:");				
-				int amount=Integer.parseInt(sc.nextLine());
-				System.out.println("Enter the date of transaction");
-				String userInput="";			
-				userInput=sc.nextLine();
-				Date dt=ChangeToDate(userInput);  
-				Transaction trans=new Transaction(type,dt,amount);
-				Statement st = conn.createStatement();
-		        String query="insert into evilcorptrans values(transaction_seq.nextval,"+Integer.parseInt(acct_number)+","+trans.getAmount()+","+trans.getTransaction_type()+","+"to_date('"+userInput+"','mm/dd/yyyy'),1)" ;   
-		        System.out.println(query);
-		        st.execute(query);
-		        cust.addToTransactions(trans);
-				 System.out.println("Do you want to process another transaction(Y/N):");
-		         choice=sc.nextLine();
+	        	 result  = preStatement.executeQuery();		
+		        Customer cust=null;	       
+		        while(result.next()){
+		        	  cust=new Customer(result.getInt("acc_num"),result.getString("Name"),result.getInt("start_bal"),result.getDate("birthday"));
+		        	 System.out.println("customer Information:\n\n"+cust);
+		        }		        
+		        String sql1="select * from evilcorptrans where user_id='"+acct_number+"' and trans_processed=0";		        
+		        PreparedStatement preStatement1 = conn.prepareStatement(sql1);		       
+		        ResultSet  result2 = preStatement1.executeQuery();	       
+		        System.out.println();
+		        while(result2.next()){
+		        	Transaction t=new Transaction(result2.getInt("trans_type"),result2.getDate("trans_date"),result2.getInt("amount"));		        	
+		        	cust.addToTransactions(t);
+		        	System.out.println(t);	        	 
+		        }
+		        if(cust==null)System.out.println("help");
+		        if(cust.getBalance()!=0)
+		           cust.updateBalance();
+		        System.out.println("Current balance= "+cust.getBalance());	           
+	            System.out.println("Do you want to process aother transaction(Y/N):");
+	            String choice=sc.nextLine();
+	            int type=0;
+	            while(choice.equalsIgnoreCase("y")){
+	        	    System.out.println("Enter a transaction type (Check-1, Debit card-2, Deposit-3 or Withdrawal-4 or CloseAccount=5):");
+	        	    type=Integer.parseInt(sc.nextLine());
+	        	    if(type!=5){
+		        	    System.out.println("Enter the amount:");				
+						int amount=Integer.parseInt(sc.nextLine());
+						System.out.println("Enter the date of transaction");
+						String userInput="";			
+						userInput=sc.nextLine();
+						Date dt=ChangeToDate(userInput);  
+						Transaction trans=new Transaction(type,dt,amount);
+						Statement st = conn.createStatement();
+				        String query="insert into evilcorptrans values(transaction_seq.nextval,"+Integer.parseInt(acct_number)+","+trans.getAmount()+","+trans.getTransaction_type()+","+"to_date('"+userInput+"','mm/dd/yyyy'),1)" ; 				        
+				        st.execute(query);
+				        cust.addToTransactions(trans);
+						 System.out.println("Do you want to process another transaction(Y/N):");
+				         choice=sc.nextLine();
+	        	    }
+	        	    else
+	        	    	choice="n";
+	           }
+          // if(type!=5){
+	           cust.updateBalance();
+	           Statement stmt = conn.createStatement();
+	           String query1="update evilcorpcustomer set start_bal="+cust.getBalance()+" where acc_num='"+acct_number+"'";           
+	           stmt.execute(query1);          
+	           String query2="update evilcorptrans set trans_processed=1 where user_id='"+acct_number+"'";
+	           stmt.execute(query2); 
+	           System.out.println("Current balance= "+cust.getBalance());
+          // }
+           if(type==5){
+        	   if(cust.getBalance()==0){ 
+        		   Statement state=conn.createStatement(); 
+        		   String qy="delete from evilcorpcustomer where acc_num="+acct_number; 
+        		   String qy1="delete from evilcorptrans where user_id="+acct_number; 
+        		   state.execute(qy); 
+        		   state.execute(qy1);
+        		   System.out.println("Account Closed.Thank You.");
+        		   } 
+        		   else if(cust.getBalance()>0) 
+        		   System.out.println("Cannot close account.Non-zero balance.Withdraw current balance and try again!"); 
+        		   else 
+        		   System.out.println("Cannot close acount due to negative balance.Pay dues and try again!"); 
+
            }
-           
-           cust.updateBalance();
-           Statement stmt = conn.createStatement();
-           String query1="update evilcorpcustomer set start_bal="+cust.getBalance()+" where acc_num='"+acct_number+"'";           
-           stmt.execute(query1);          
-           String query2="update evilcorptrans set trans_processed=1 where user_id='"+acct_number+"'";
-           stmt.execute(query2); 
-           System.out.println("Current balance= "+cust.getBalance());
 	    }
            conn.close();
            sc.close();
